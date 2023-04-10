534d074fe390c24db21833e20cc4b84545915133
Yanyo <jeanp.matgal@educa.jcyl.es>
2023-04-10 18:51:20 +0200

    Segundo Commit

--- EclipseGit1/src/Proyecto1/Principal1.java
+++ EclipseGit1/src/Proyecto1/Principal1.java
@@ -2,6 +2,9 @@ package Proyecto1;
 
 public class Principal1 {
 	public static void main(String[] args) {
-		System.out.print("Hola git");
+		System.out.print("Hola git nuevo");
+		System.out.print("Esto es nuevo");
+		System.out.print("Hola, soy Jean Paul");
+		
 	}
 }
