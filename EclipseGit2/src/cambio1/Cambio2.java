7659ae6a370d39ec6a277330ca29f1d170546c69
Yanyo <jeanp.matgal@educa.jcyl.es>
2023-04-12 19:17:55 +0200

    Primer Commit Clon

---
+++ EclipseGit2/src/cambio1/Cambio2.java
@@ -0,0 +1,8 @@
+package cambio1;
+
+public class Cambio1 {
+	public static void main(String[] args) {
+		System.out.print("Pruebas Git.Clase Cambio1S");
+		System.out.print("Estoy el proyecto GitClone");
+	}
+}
